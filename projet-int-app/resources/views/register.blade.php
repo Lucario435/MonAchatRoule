9dd363319ecff4e25489c8e8f2f28060056f25fa
Chahine-Benramoul <chahinebenramoul58@gmail.com>
2023-09-23 11:59:28 -0400

    t

--- projet-int-app/resources/views/register.blade.php
+++ projet-int-app/resources/views/register.blade.php
@@ -40,13 +40,13 @@
         <div class="d-flex h-15 justify-content-center p-1  text-white">
           <div class="p-2 ">
             <span class="blue-block icon icon-password"></span>
-            <input type="text" placeholder="Mot de passe" name="password" id="password" required>
+            <input type="password" placeholder="Mot de passe" name="password" id="password" required>
           </div>
         </div>
         <div class="d-flex h-15 justify-content-center p-1  text-white">
           <div class="p-2 ">
             <span class="blue-block icon icon-password"></span>
-            <input type="text" placeholder="Confirmez le mot de passe" name="password_confirm" id="password_confirm" required>
+            <input type="password" placeholder="Confirmez le mot de passe" name="password_confirm" id="password_confirm" required>
           </div>
         </div>
         <div class="d-flex h-15 justify-content-center p-1  text-white">
@@ -60,29 +60,7 @@
             <input type="submit" value="Créer">
           </div>
         </div>
-
-        {{-- <div class="container signForm">
-            <p class="petitp">Veuillez remplir vos renseignements</p>
-            <br>
-            <label for="email"><b>Courriel</b></label>
-            <input type="text" placeholder="Entrez votre courriel" name="email" id="email" required>
-            <label for="rusername"><b>Nom d'utilisateur</b></label>
-            <input type="text" placeholder="Entrez votre nom d'utilisateur" name="rusername" id="rusername" required>
-
-            <label for="psw"><b>Mot de passe</b></label>
-            <input type="password" placeholder="Entrez votre mot de passe" name="psw" id="psw" required>
-            <label  for="pswc"><b>Confirmation de mot de passe</b></label>
-            <input type="password" placeholder="Confirmez votre mot de passe" name="pswc" id="pswc" required>
-            <br> <br>
-            <label for="emailnotifs"><b>Notifications Par Courriel?</b></label>
-            <select name="emailnotifs" id="emailnotifs">
-                <option value="yes" selected>Oui</option>
-                <option value="no">Non</option>
-            </select>
-            <br> <br>
-            <button type="submit" class="registerbtn">Confirmer</button>
-        </div> --}}
-
+        <hr style="width:50%; margin:5 auto 5 auto;">
         <div class="signForm signin">
             <p>Vous avez déjà un compte?<a href="{{ url("/login") }}"> Connectez vous</a>.</p>
         </div>
