cf77c97937c5b625251a1b8442f83c1122af1ad4
Chahine-Benramoul <chahinebenramoul58@gmail.com>
2023-09-23 19:07:12 -0400

    table users

--- projet-int-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ projet-int-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
             $table->string('name');
+            $table->string('surname');
+            $table->string('phone');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
