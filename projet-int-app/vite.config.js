32334107e30a232d0ee1b9e319886e26b9335fd7
Chahine-Benramoul <chahinebenramoul58@gmail.com>
2023-10-08 22:23:53 -0400

    refresh all views including repositories in it

--- projet-int-app/vite.config.js
+++ projet-int-app/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
             refresh: [
                 'resources/routes/**',
                 'routes/**',
-                'resources/views/*',
+                'resources/views/**',
             ],
             server:{
                 host: '127.0.0.1:8000',
