e28d60b16b493a90261df0a679af5e3899c43803
Chahine-Benramoul <chahinebenramoul58@gmail.com>
2023-11-22 13:07:29 -0500

    added .js

--- projet-int-app/resources/js/ImageControl.js
+++ projet-int-app/resources/js/ImageControl.js
@@ -55,7 +55,7 @@
 //  <span class="field-validation-valid text-danger" data-valmsg-for="PhotoImageData" data-valmsg-replace="true"></span>
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-import './validation';
+import './validation.js';
 let missingFileErrorMessage = "Veuillez sélectionner une image.";
 let maxImageSize = 15000000;
 var currentId = 0;
