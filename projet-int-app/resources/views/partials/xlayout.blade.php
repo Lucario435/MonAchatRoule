605c26902f4ee87e4b417afa37779dfe25827187
Chahine-Benramoul <chahinebenramoul58@gmail.com>
2023-11-22 13:10:21 -0500

    added spacing from header, bc its snaps to top

--- projet-int-app/resources/views/partials/xlayout.blade.php
+++ projet-int-app/resources/views/partials/xlayout.blade.php
@@ -150,7 +150,8 @@
 <body>
     @include('partials.xheader')
 
-
+    <div style="min-height:90px;"></div>
+    
     <h1 id="xtitle">
         @yield('title'){{-- ici on mettra le nom de la page, doit etre défini dans le yield  --}}
     </h1>
