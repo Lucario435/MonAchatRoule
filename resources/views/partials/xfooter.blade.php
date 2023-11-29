a71595b3dca5be74002eb27ea247d94a398fecb6
Lucario435 <74693472+Lucario435@users.noreply.github.com>
2023-11-29 00:02:27 -0500

    footer + admin fixed

--- resources/views/partials/xfooter.blade.php
+++ resources/views/partials/xfooter.blade.php
@@ -1,7 +1,12 @@
 <br>
 <br>
 <br>
-<footer style="height:7em;width:100%; background-image: linear-gradient(to right, #001b3d, #004aad);align-items:center;">
+<footer style="height: 7em;
+width: 100%;
+position: relative;
+z-index: 100;
+background-image: linear-gradient(to right, rgb(0, 27, 61), rgb(0, 74, 173));
+align-items:center;">
     <br>
     <ul style="margin:auto; grid-template-columns: repeat( auto-fit, minmax(50px, 1fr) ); text-align:center;color:white;" class="nav justify-content-center">
         <li style="color:white;" class="nav-item"><a href="{{route('publication.index')}}" class="nav-link px-2" style="color:white;">Trouver un véhicule</a></li>
