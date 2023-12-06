7b19e49ecb6c2a5d158eb7dcd69c81ca631feeb7
Chahine-Benramoul <chahinebenramoul58@gmail.com>
2023-12-06 00:23:56 -0500

    typo

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -19,8 +19,8 @@
         <div id="container-signalements">@include('admin.list-signalements')</div>
     </div>
     <div class="modal fade" id="signalerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" style="max-height:80%">
-            <div class="modal-content">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content" style="max-height:80%">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="exampleModalLabel">Traiter le signalement</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
